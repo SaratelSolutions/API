ec550be88003a427fca4af2379e59d5e8e109078
ssigab <48002574+ssigab@users.noreply.github.com>
2019-03-25 09:08:51 +1100

    Update index.php.php

--- v2/index.php.php
+++ v2/index.php.php
@@ -2,7 +2,7 @@
 $teststring = "add";
 $url = "https://saratel.atlassian.net/rest/api/2/issue";
 $username = "gabriel.alforque@anaca.com.au";
-$password = "154263789aA";
+$password = "******************";
 $txt = '{
     "fields": {
         "project": {
